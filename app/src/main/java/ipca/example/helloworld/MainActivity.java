f09a2f932af879496b980504e0dedd6f287af439
Lourenço <l.gomes@winwel.com>
2017-09-25 17:24:43 +0100

    Added share button

--- app/src/main/java/ipca/example/helloworld/MainActivity.java
+++ app/src/main/java/ipca/example/helloworld/MainActivity.java
@@ -1,16 +1,25 @@
 package ipca.example.helloworld;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonEnglish;
     Button buttonPortuguese;
+    Button buttonShare;
     TextView textViewTranslation;
 
     String dataToTranslate = ""; // model
@@ -31,6 +40,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonEnglish.setOnClickListener(this);
         buttonPortuguese = (Button)findViewById(R.id.buttonPortuguese);
         buttonPortuguese.setOnClickListener(this);
+        buttonShare = (Button)findViewById(R.id.buttonShare);
+        buttonShare.setOnClickListener(this);
+
 
         updateView();
     }
@@ -47,6 +59,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.buttonEnglish:
                 dataToTranslate = "Hello World!";
+                break;
+            case R.id.buttonShare:
+                Intent shareIntent;
+                shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Prova Oral Podcast");
+                shareIntent.putExtra(Intent.EXTRA_TEXT,
+                                "https://play.google.com/store/apps/details?id=osga.util.ProvaOral");
+                startActivity(Intent.createChooser(shareIntent, "Partilhar"));
+
                 break;
         }
         updateView();
